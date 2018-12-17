a7b8c4b3ec87480a7a15779c2c8dafefc5335e10
Simon Kardous <simon.kardous@ubisoft.com>
2018-12-17 10:49:11 +0100

    Script test

---
+++ Assets/Scripts/first.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class first : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
